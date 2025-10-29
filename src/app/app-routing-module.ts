7e56b17566a8c09dc1e67598ed862ab1a5d076fd
Heber Munaylla Gutierrez <heber.munaylla@gmail.com>
2025-10-29 15:40:52 -0500

    Incluir Routing y Formulario Login

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './featured/auth/login/login';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'login',
+    component: Login,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -3,9 +3,23 @@ import { CommonModule } from '@angular/common';
 import { CapitalizePipe } from './pipes/capitalize/capitalize-pipe';
 import { SeparateLettersPipe } from './pipes/separate-letters/separate-letters-pipe';
 import { Resaltado } from './directives/resaltado/resaltado';
-
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { provideNativeDateAdapter } from '@angular/material/core';
 @NgModule({
   declarations: [
     CapitalizePipe,
@@ -13,10 +27,44 @@ import { Resaltado } from './directives/resaltado/resaltado';
     Resaltado
   ],
   imports: [
-    CommonModule
+      CommonModule,
+      ReactiveFormsModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatButtonModule,
+      MatIconModule,
+      MatNavList,
+      MatToolbar,
+      MatSidenavModule,
+      MatListModule,
+      MatDividerModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule,
+      MatDatepickerModule,
+      MatSelectModule,
   ],
   exports: [
-    CapitalizePipe, SeparateLettersPipe, Resaltado
-  ]
+    CapitalizePipe, SeparateLettersPipe, Resaltado,
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatNavList,
+    MatToolbar,
+    MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDatepickerModule,
+    MatSelectModule,
+  ],
+  providers: [provideNativeDateAdapter()],
 })
 export class SharedModule { }
