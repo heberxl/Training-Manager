dec2b873f1c284a5120f7a4a8a04d59c29bc648b
Heber Munaylla Gutierrez <heber.munaylla@gmail.com>
2025-10-29 17:25:56 -0500

    Añadir un dashboard con sus  módulos, componentes y routes respectivos

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './featured/auth/login/login';
+import { Dashboard } from './featured/dashboard/dashboard';
 
 const routes: Routes = [
   { 
     path: 'login',
     component: Login,
   },
-
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    loadChildren: () => import('./featured/dashboard/dashboard-module').then(m => m.DashboardModule)
+  }
 ];
 
 @NgModule({
