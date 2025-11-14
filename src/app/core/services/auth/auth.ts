42449824dedd0235c1a2fb6b081e7f8b5206b87c
Heber Munaylla Gutierrez <heber.munaylla@gmail.com>
2025-11-14 11:45:40 -0500

    Añadir login para ingresar al dashboard

---
+++ src/app/core/services/auth/auth.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  login(email: string, password: string) {
+    if (email !== 'admin@gmail.com' || password !== '1234') {
+      throw new Error('Email y contraseña incorrectos');
+    }
+    localStorage.setItem('token', 'admin@gmail.com');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isAuthenticated() {
+    //return localStorage.getItem('token') !== null;
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return false;
+    }
+    return token === 'admin@gmail.com';
+  }
+}
--- src/app/featured/auth/login/login.ts
+++ src/app/featured/auth/login/login.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +11,26 @@ import { Component } from '@angular/core';
 })
 export class Login {
 
+  loginForm: FormGroup;
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]});
+  }
+
+  onLogin() {
+    if (!this.loginForm.valid) {
+      alert('Formulario no válido');
+      return;
+    }
+
+    try {
+      this.authService.login(this.loginForm.value.email, this.loginForm.value.password);
+      this.router.navigate(['/dashboard']);
+    } catch (error: any) {
+      console.log(error);
+      alert(error);
+    }
+  }
+  
 }
